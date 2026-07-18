473427c71631071119305ae9bb407e8e5e7f2795
Tokioshi <grizzlizyping@gmail.com>
2026-07-18 11:54:22 +0700

    Add ban into command list for registry

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,10 +2,11 @@ import watch from "./watch.js";
 import unwatch from "./unwatch.js";
 import list from "./list.js";
 import clear from "./clear.js";
+import ban from "./ban.js";
 
 // To add a new command: create commands/<name>.js exporting
 // { name, definition, execute, autocomplete? }, then register it here.
-const all = [watch, unwatch, list, clear];
+const all = [watch, unwatch, list, clear, ban];
 
 export const COMMANDS = Object.fromEntries(
     all.map((command) => [
