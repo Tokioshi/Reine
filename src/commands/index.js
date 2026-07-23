471d645f6eaa8398718dbeba48eb3d59637bc910
Tokioshi <grizzlizyping@gmail.com>
2026-07-23 23:40:20 +0700

    Registering modal and map all modal

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,10 +3,11 @@ import unwatch from "./unwatch.js";
 import list from "./list.js";
 import clear from "./clear.js";
 import ban from "./ban.js";
+import message from "./message.js";
 
 // To add a new command: create commands/<name>.js exporting
 // { name, definition, execute, autocomplete? }, then register it here.
-const all = [watch, unwatch, list, clear, ban];
+const all = [watch, unwatch, list, clear, ban, message];
 
 export const COMMANDS = Object.fromEntries(
     all.map((command) => [
@@ -16,3 +17,9 @@ export const COMMANDS = Object.fromEntries(
 );
 
 export const slashCommands = all.map(({ name, definition }) => ({ name, ...definition }));
+
+export const MODAL_HANDLERS = Object.fromEntries(
+    all
+        .filter((command) => command.modalId)
+        .map((command) => [command.modalId, command.handleModalSubmit]),
+);
