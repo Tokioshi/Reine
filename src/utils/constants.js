a0be81001d7b79cfaa491cb052ee8d577ead1586
Tokioshi <grizzlizyping@gmail.com>
2026-07-23 23:41:10 +0700

    Add component type and input style for modal

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@ export const InteractionType = {
     PING: 1,
     APPLICATION_COMMAND: 2,
     APPLICATION_COMMAND_AUTOCOMPLETE: 4,
+    MODAL_SUBMIT: 5,
 };
 
 export const InteractionResponseType = {
@@ -9,6 +10,18 @@ export const InteractionResponseType = {
     CHANNEL_MESSAGE_WITH_SOURCE: 4,
     DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE: 5,
     APPLICATION_COMMAND_AUTOCOMPLETE_RESULT: 8,
+    MODAL: 9,
+};
+
+export const ComponentType = {
+    TEXT_INPUT: 4,
+    LABEL: 18,
+    FILE_UPLOAD: 19,
+};
+
+export const TextInputStyle = {
+    SHORT: 1,
+    PARAGRAPH: 2,
 };
 
 export const MessageFlags = {
