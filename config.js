f80f7af521d9b38aeed74211005317780a014af2
Tokioshi <grizzlizyping@gmail.com>
2026-09-23 22:35:26 +0700

    Defining color embd to be imported on other files

---
+++ config.js
@@ -0,0 +1,12 @@
+export default {
+    color: {
+        success: 0x0E9F6E,
+        error: 0xE11D48,
+        warning: 0xD97706,
+        information: 0x2563EB,
+        neutral: 0x64748B,
+        processing: 0x7C3AED,
+        offline: 0x6B7280,
+        critical: 0xB91C1C
+    }
+}
